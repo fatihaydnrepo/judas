a9d3686f8789bbb6d7e22bf94800ed2faeae3ce4
Fatih AYDIN <128099981+fatihaydnrepo@users.noreply.github.com>
2024-11-19 22:38:36 +0300

    Update update-kubeconfig.sh

--- scripts/update-kubeconfig.sh
+++ scripts/update-kubeconfig.sh
@@ -5,7 +5,7 @@ GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 
-# IP adresini al ya da varsayılan olarak 192.168.1.50 kullan
+
 HOST_IP=${1:-"192.168.1.103"}
 PORT=${2:-"6444"}
 
@@ -14,7 +14,7 @@ echo -e "${BLUE}Updating kubeconfig with host IP: $HOST_IP and port: $PORT${NC}"
 # Geçici dosyayı oluştur
 kind get kubeconfig --name test-cluster > /tmp/kube_temp
 
-# IP ve port'u güncelle, TLS doğrulamasını devre dışı bırak
+# IP ve port'u güncelle
 cat /tmp/kube_temp | \
     sed "s|server: https://.*|server: https://$HOST_IP:$PORT|g" | \
     sed 's|certificate-authority-data:.*|insecure-skip-tls-verify: true|g' > /tmp/kube_final
